59e1c15962791c361b7db36142a4d1c39ea3cbb2
unknown <chanhi421@naver.com>
2025-12-25 02:52:22 +0900

    add idea card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -2,64 +2,92 @@ import { Link, type MetaFunction } from "react-router";
 import { ProductCard } from "~/features/users/components/product-card";
 import { Button } from "../components/ui/button";
 import { PostCard } from "~/features/community/components/post-card";
+import { IdeaCard } from "~/features/ideas/components/idea-card";
 
 export const meta: MetaFunction = () => {
-    return [
-        {title: "Home | wemake"},
-        {name: "description", content: "Welcome to Wemake"},
-    ]
+  return [
+    { title: "Home | wemake" },
+    { name: "description", content: "Welcome to Wemake" },
+  ];
 };
 
 export default function HomePage() {
-    return(
-        <div className="px-20 space-y-40">
-            <div className="grid grid-cols-3 gap-4">
-                <div>
-                    <h2 className="text-5xl font-bold leading-tight tracking-tight">
-                        Today's Products
-                    </h2>
-                    <p className="text-xl font-light text-foreground">
-                        The best products made by our community today.
-                    </p>
-                    <Button variant="link" className="text-lg p-0">
-                        <Link to="/products/leaderboard">See All Product &rarr;</Link>
-                    </Button>
-                </div>
-                {Array.from({ length: 11 }).map((_, index) => (
-                    <ProductCard
-                        id={`productId-${index}`} 
-                        name="Product Name" 
-                        description="Product Description" 
-                        commentsCount={12}
-                        viewsCount={12}
-                        votesCount={120}
-                    />
-                ))}
-            </div>
-
-            <div className="grid grid-cols-3 gap-4">
-                <div>
-                    <h2 className="text-5xl font-bold leading-tight tracking-tight">
-                        Latest Discussions
-                    </h2>
-                    <p className="text-xl font-light text-foreground">
-                        The latest discussions from our community.
-                    </p>
-                    <Button variant="link" className="text-lg p-0">
-                        <Link to="/community">Explore all discussions &rarr;</Link>
-                    </Button>
-                </div>
-                {Array.from({ length: 11 }).map((_, index) => (
-                    <PostCard
-                        id={`postId-${index}`}
-                        title="What is the best productivity tool?"
-                        author="Author Name"
-                        authorAvatarUrl="https://github.com/apple.png"
-                        category="Productivity"
-                        postedAt="3 days ago"
-                    />
-                ))}
-            </div>
+  return (
+    <div className="px-20 space-y-40">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our community today.
+          </p>
+          <Button variant="link" className="text-lg p-0">
+            <Link to="/products/leaderboard">See All Product &rarr;</Link>
+          </Button>
         </div>
-    );
+        {Array.from({ length: 11 }).map((_, index) => (
+          <ProductCard
+            key={`productId-${index}`}
+            id={`productId-${index}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Discussions
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The latest discussions from our community.
+          </p>
+          <Button variant="link" className="text-lg p-0">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <PostCard
+            key={`postId-${index}`}
+            id={`postId-${index}`}
+            title="What is the best productivity tool?"
+            author="Author Name"
+            authorAvatarUrl="https://github.com/apple.png"
+            category="Productivity"
+            postedAt="3 days ago"
+          />
+        ))}
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            IdeasGPT
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            Find ideas for your next project.
+          </p>
+          <Button variant="link" className="text-lg p-0">
+            <Link to="/ideas">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <IdeaCard
+            key={`ideaId-${index}`}
+            id={`ideaId-${index}`}
+            title="A startup that creates an AI-powered generated personal trainer, delivering customized fitness recommendations and tracking of progress using a mobile app to track workouts and progress as well as a website to manage the business."
+            viewsCount={124}
+            postedAt="3 days ago"
+            likesCount={120}
+            claimed={index % 2 === 0}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
