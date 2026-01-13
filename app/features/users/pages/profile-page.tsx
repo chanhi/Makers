6930b1c7c6b7b69b1f0f0f9630f7cb527f430b1e
User <chanhi421@naver.com>
2026-01-13 21:26:49 +0900

    make user routes and files with notification and settings

--- app/features/users/pages/profile-page.tsx
+++ app/features/users/pages/profile-page.tsx
@@ -1,3 +1,9 @@
+import type { Route } from "./+types/profile-page";
+
+export const meta: Route.MetaFunction = () => {
+  return [{ title: "User Profile | wemake" }];
+};
+
 export default function ProfilePage() {
-    return <h1>Profile</h1>;
+  return <h1>Profile</h1>;
 }
