0cfe899d668e5e50b34df48e3968b2b5b9ca3866
User <chanhi421@naver.com>
2026-01-28 19:35:32 +0900

    supabase seetings

--- app/features/community/queries.ts
+++ app/features/community/queries.ts
@@ -1,3 +1,5 @@
+import client from "~/supa-client";
+/*
 import db from "~/db";
 import { posts, postUpvotes, topics } from "./schema";
 import { asc, count, desc, eq } from "drizzle-orm";
@@ -38,4 +40,33 @@ export const getPosts = async () => {
     )
     .orderBy(asc(posts.post_id));
   return allPosts;
+};
+*/
+
+export const getTopics = async () => {
+  const { data, error } = await client.from("topics").select("name, slug");
+  if (error) throw new Error(error.message);
+  return data;
+};
+
+export const getPosts = async () => {
+  const { data, error } = await client.from("posts").select(`
+      post_id,
+      title,
+      created_at,
+      topic:topics!inner (
+        name
+      ),
+      author:profiles!posts_profile_id_profiles_profile_id_fk!inner (
+        name,
+        username,
+        avatar
+      ),
+      upvotes:post_upvotes (
+        count
+      )
+  `);
+  console.log(error);
+  if (error) throw new Error(error.message);
+  return data;
 };
