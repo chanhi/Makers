10f3363acc213e644abc173cfcddc9cb77649be4
User <chanhi421@naver.com>
2026-02-22 23:00:00 +0900

    link category's data

--- app/features/products/pages/categories-page.tsx
+++ app/features/products/pages/categories-page.tsx
@@ -1,30 +1,29 @@
 import { Hero } from "~/common/components/hero";
 import type { Route } from "./+types/categories-page";
 import { CategoryCard } from "../components/category-card";
-import { ChevronRightIcon } from "lucide-react";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/common/components/ui/card";
+import { getCategories } from "../queries";
 
 export const meta: Route.MetaFunction = () => [
   { title: "Categories | wemake" },
   { name: "description", content: "Browse product categories" },
 ];
 
-export default function CategoriesPage() {
+export const loader = async () => {
+  const categories = await getCategories();
+  return { categories };
+};
+
+export default function CategoriesPage({ loaderData }: Route.ComponentProps) {
   return (
     <div className="space-y-10">
       <Hero title="Categories" subtitle="Browse product categories" />
       <div className="grid grid-cols-4 gap-10">
-        {Array.from({ length: 10 }).map((_, index) => (
+        {loaderData.categories.map((category) => (
           <CategoryCard
-            key={`categoryId-${index}`}
-            id={`categoryId-${index}`}
-            name={`Category ${index + 1}`}
-            description={`Description for Category ${index + 1}`}
+            key={category.category_id}
+            id={category.category_id}
+            name={category.name}
+            description={category.description}
           />
         ))}
       </div>
