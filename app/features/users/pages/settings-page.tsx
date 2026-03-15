8fac2416b68455ede9477c1dbc5bbed6830489b4
User <chanhi421@naver.com>
2026-03-15 18:36:38 +0900

    upload flie in profile

--- app/features/users/pages/settings-page.tsx
+++ app/features/users/pages/settings-page.tsx
@@ -9,7 +9,7 @@ import { Button } from "~/common/components/ui/button";
 import { makeSSRClient } from "~/supa-client";
 import { getLoggedInUser, getUserById } from "../queries";
 import z from "zod";
-import { updateUser } from "../mutations";
+import { updateUser, updateUserAvatar } from "../mutations";
 import {
   Alert,
   AlertDescription,
@@ -38,35 +38,60 @@ export const action = async ({ request }: Route.ActionArgs) => {
   const { client } = makeSSRClient(request);
   const userId = await getLoggedInUser(client);
   const formData = await request.formData();
-  const { success, error, data } = formSchema.safeParse(
-    Object.fromEntries(formData),
-  );
-  if (!success) {
-    return { formErrors: error.flatten().fieldErrors };
+  const avatar = formData.get("avatar");
+  if (avatar && avatar instanceof File) {
+    if (avatar.size <= 2097152 && avatar.type.startsWith("image/")) {
+      const { data, error } = await client.storage
+        .from("avatars")
+        .upload(`${userId}/${Date.now()}`, avatar, {
+          contentType: avatar.type,
+          upsert: true,
+        });
+      if (error) {
+        console.log(error);
+        return { formErrors: { avatar: ["Failed to upload avatar"] } };
+      }
+      const {
+        data: { publicUrl },
+      } = await client.storage.from("avatars").getPublicUrl(data.path);
+      await updateUserAvatar(client, {
+        id: userId,
+        avatarUrl: publicUrl,
+      });
+    } else {
+      return { formErrors: { avatar: ["Invalid file size or type"] } };
+    }
+  } else {
+    const { success, error, data } = formSchema.safeParse(
+      Object.fromEntries(formData),
+    );
+    if (!success) {
+      return { formErrors: error.flatten().fieldErrors };
+    }
+    const { name, role, headline, bio } = data;
+    await updateUser(client, {
+      id: userId,
+      name,
+      role: role as
+        | "developer"
+        | "designer"
+        | "marketer"
+        | "founder"
+        | "product-manager",
+      headline,
+      bio,
+    });
+    return {
+      ok: true,
+    };
   }
-  const { name, role, headline, bio } = data;
-  await updateUser(client, {
-    id: userId,
-    name,
-    role: role as
-      | "developer"
-      | "designer"
-      | "marketer"
-      | "founder"
-      | "product-manager",
-    headline,
-    bio,
-  });
-  return {
-    ok: true,
-  };
 };
 
 export default function SettingsPage({
   loaderData,
   actionData,
 }: Route.ComponentProps) {
-  const [avatar, setAvatar] = useState<string | null>(null);
+  const [avatar, setAvatar] = useState<string | null>(loaderData.user.avatar);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const file = event.target.files[0];
@@ -88,11 +113,11 @@ export default function SettingsPage({
               name="name"
               placeholder="John Doe"
             />
-            {actionData?.formErrors?.name ? (
+            {actionData?.formErrors && "name" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.name.join(", ")}
+                  {actionData.formErrors?.name?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -110,11 +135,11 @@ export default function SettingsPage({
                 { label: "Marketer", value: "marketer" },
               ]}
             />
-            {actionData?.formErrors?.role ? (
+            {actionData?.formErrors && "role" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.role.join(", ")}
+                  {actionData.formErrors?.role?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -128,11 +153,11 @@ export default function SettingsPage({
               placeholder="John Doe"
               textarea
             />
-            {actionData?.formErrors?.headline ? (
+            {actionData?.formErrors && "headline" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.headline.join(", ")}
+                  {actionData.formErrors?.headline?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -146,11 +171,11 @@ export default function SettingsPage({
               placeholder="John Doe"
               textarea
             />
-            {actionData?.formErrors?.bio ? (
+            {actionData?.formErrors && "bio" in actionData?.formErrors ? (
               <Alert>
                 <AlertTitle>Error</AlertTitle>
                 <AlertDescription>
-                  {actionData.formErrors.bio.join(", ")}
+                  {actionData.formErrors?.bio?.join(", ")}
                 </AlertDescription>
               </Alert>
             ) : null}
@@ -165,7 +190,11 @@ export default function SettingsPage({
             </Alert>
           ) : null}
         </div>
-        <aside className="col-span-2 p-6 rounded-lg border shadow-md">
+        <Form
+          className="col-span-2 p-6 rounded-lg border shadow-md"
+          method="post"
+          encType="multipart/form-data"
+        >
           <Label className="flex flex-col gap-1">
             Avatar
             <small className="text-muted-foreground">
@@ -183,8 +212,16 @@ export default function SettingsPage({
               className="w-1/2"
               onChange={onChange}
               required
-              name="icon"
+              name="avatar"
             />
+            {actionData?.formErrors && "avatar" in actionData?.formErrors ? (
+              <Alert>
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>
+                  {actionData.formErrors.avatar.join(", ")}
+                </AlertDescription>
+              </Alert>
+            ) : null}
             <div className="flex flex-col text-xs">
               <span className=" text-muted-foreground">
                 Recommended size: 128x128px
@@ -196,7 +233,7 @@ export default function SettingsPage({
             </div>
             <Button className="w-full">Update avatar</Button>
           </div>
-        </aside>
+        </Form>
       </div>
     </div>
   );
